bf890a1fefc01c4888b5f8dcd41b424a847cf439
Dj2035 <dk.jude@yahoo.com>
2022-09-08 21:01:40 +0200

    Added component that renders calender events objects in EventList as described in project brief

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    show: false
+  };
+
+  toggleEventDetails = () => {
+    this.setState({ show: !this.state.show });
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return (
+      <div>
+        <h1 className="event-title">{event.summary}</h1>
+        <p className="event-default-info">
+          {event.start.dateTime} {event.start.timeZone} {event.location}
+        </p>
+        {this.state.show && (
+          <>
+            <h2 className="event-about-title">About event:</h2>
+            <a
+              href={event.htmlLink}
+              target="_blank"
+              rel="noreferrer"
+              className="event-htmlLink"
+            >
+              See details on Google Calendar
+            </a>
+            <p className="event-description">{event.description}</p>
+          </>
+        )}
+
+        {!this.state.show ? (
+          <button
+            className="event-showDetails-btn"
+            onClick={this.toggleEventDetails}
+          >
+            Show Details
+          </button>
+        ) : (
+          <button
+            className="event-hideDetails-btn"
+            onClick={this.toggleEventDetails}
+          >
+            Hide Details
+          </button>
+        )}
+
+      </div>
+    );
   }
 }
 export default Event;
