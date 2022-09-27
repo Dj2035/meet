bf1e32eca3ca953bf9db502b7dfb6ae0b59f7f91
Dj2035 <dk.jude@yahoo.com>
2022-09-27 08:07:51 +0200

    Changed getData function to reduce variation in event genre names

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,8 +4,8 @@ import {
 } from 'recharts';
 
 const EventGenre = ({ events }) => {
-  const [data, setData] = useState([]);
 
+  /*const [data, setData] = useState([]);
   useEffect(() => {
     setData(() => getData());
   }, [events]);
@@ -20,29 +20,74 @@ const EventGenre = ({ events }) => {
       };
     });
     return data;
-  }
+  }*/
 
+  const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
   const COLORS = ['#0088FE', '#00C49F', '#F5ED0A', '#FF8042', '#85D2DB'];
 
-  return (
-    <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          cx={250}
-          cy={200}
-          labelLine={false}
-          outerRadius={100}
-          fill="#8884d8"
-          dataKey="value"
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+  const pieChartData = [];
+  const genresAndCounts = {};
+  genres.forEach((genre) => {
+    genresAndCounts[genre] = 0;
+  });
+
+  events.forEach((event) => {
+    genres.forEach((genre) => {
+      if (event.summary.includes(genre)) {
+        genresAndCounts[genre]++;
+      }
+    });
+  });
+
+  Object.keys(genresAndCounts).forEach((genre) => {
+    pieChartData.push({ name: genre, value: genresAndCounts[genre] });
+  });
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 1.1;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    if (percent > 0) {
+      return (
+        <text
+          x={x}
+          y={y}
+          fill="black"
+          textAnchor={x > cx ? "start" : "end"}
+          dominantBaseline="central"
         >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} name={entry.name} />
-          ))}
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
+          {`${(percent * 100).toFixed(0)}% ${genres[index]}`}
+        </text>
+      );
+    }
+  };
+
+  return (
+    <PieChart width={600} height={600}>
+      <Pie
+        data={pieChartData}
+        cx={"50%"}
+        cy={"50%"}
+        labelLine={false}
+        outerRadius={150}
+        fill="#8884d8"
+        dataKey="value"
+        label={renderCustomizedLabel}
+      >
+        {pieChartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+    </PieChart>
   );
 }
 
